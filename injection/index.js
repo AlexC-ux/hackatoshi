f1af8e13e4b30c5e8cdca5cfa1f3953188d95244
Krugmor <mr.max.gwt@gmail.com>
2023-02-17 22:40:26 +0300

    prettify

--- injection/index.js
+++ injection/index.js
@@ -1,21 +1,27 @@
-function initSmartHelper() {
-  window.smarthelper = {};
-  smarthelper.rootWindow = window;
-  smarthelper.rootDocument = smarthelper.rootWindow.document;
-
-  var b = smarthelper.rootDocument.getElementsByTagName("body")[0];
+function initStyles() {
   var h = smarthelper.rootDocument.getElementsByTagName("head")[0];
-
   var s = document.createElement("style");
   s.innerHTML =
     ".smarthelper-hint{position: fixed;bottom: 10px;right: 10px;border: none;width: 400px;height: 350px;}";
   h.appendChild(s);
+}
 
+function initPageHintFrame() {
+  var b = smarthelper.rootDocument.getElementsByTagName("body")[0];
   var f = document.createElement("iframe");
   f.setAttribute("src", "http://localhost:3000/");
   f.setAttribute("id", "smarthelper-hint");
   f.setAttribute("class", "smarthelper-hint");
   b.appendChild(f);
+}
+
+function initSmartHelper() {
+  window.smarthelper = {};
+  smarthelper.rootWindow = window;
+  smarthelper.rootDocument = smarthelper.rootWindow.document;
+
+  initStyles();
+  initPageHintFrame();
 }
 
 !(function () {
