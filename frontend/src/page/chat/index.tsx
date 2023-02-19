60749d089c57c93c5e752a32a871d91d55e10987
Krugmor <mr.max.gwt@gmail.com>
2023-02-19 04:09:17 +0300

    prepare to show

--- frontend/src/page/chat/index.tsx
+++ frontend/src/page/chat/index.tsx
@@ -3,5 +3,7 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 export const ChatPage: React.FC = () => {
-  return <main className={styles["chat-page"]}></main>;
+  return <main className={styles["chat-page"]}>
+    
+  </main>;
 };
