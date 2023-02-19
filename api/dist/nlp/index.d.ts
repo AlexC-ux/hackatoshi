09b12975a3aabc628978ffd35a9ef06164dbe65e
Krugmor <mr.max.gwt@gmail.com>
2023-02-19 08:15:30 +0300

    add docker

--- api/dist/nlp/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class NaturalLangPr {
-    static getResult(input: string): any[];
-}
--- api/dist/sockets/authenticated-socket.adapter.d.ts
+++
@@ -1,8 +0,0 @@
-import { INestApplicationContext } from '@nestjs/common';
-import { IoAdapter } from '@nestjs/platform-socket.io';
-import { Server } from 'socket.io';
-export declare class AuthenticatedSocketAdapter extends IoAdapter {
-    private app;
-    constructor(app: INestApplicationContext);
-    createIOServer(port: number, options?: any): Server<import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, import("socket.io/dist/typed-events").DefaultEventsMap, any>;
-}
